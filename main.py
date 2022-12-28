560b03f60e89abb7362bd3623c4b5cd5dfd52711
TWTom041 <s10855041@thjh.tp.edu.tw>
2022-12-28 23:15:55 +0800

    1

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from cv2 import cv2
 from pathlib import Path
 import h5py
+import pickle
+import lzma
 import skvideo.io
 import numpy as np
 from tqdm import tqdm
@@ -8,26 +10,26 @@ from multiprocessing import Pool
 
 DIR = Path(r"C:\Users\Tom\Videos\clips")
 no_label = []
-f = h5py.File("data_cut.hdf5", "w")
 
 size = 700
 dx = 580
 dy = 300
+dsize = 175
 
 for sub in "qwertyuiopasdfghjklzxcvbnm":
     d = DIR / sub
     now_data = []
     print(sub)
     for index, clip in tqdm(enumerate(d.iterdir())):
-        video_data = skvideo.io.vread(str(d / clip), as_grey=True)[0, dy:dy+size, dx:dx+size, :]
+        video_data = skvideo.io.vread(str(d / clip), as_grey=True)[:, dy:dy + size, dx:dx + size, :]
+        video_data = np.array([cv2.resize(src, (dsize, dsize)) for src in video_data])
         if index % 2 == 0:
             no_label.append(video_data)
         else:
             now_data.append(video_data)
 
-    f.create_dataset(sub, data=now_data)
+    with lzma.open(f"{sub}.p.xz", "w") as f:
+        pickle.dump(now_data, f)
 
-f.create_dataset("no_label", np.array(no_label))
-
-
-
+with lzma.open("no_label.p.xz", "w") as f:
+    pickle.dump(no_label, f)
